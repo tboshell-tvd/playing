f777ebdbc514ac443e62bfa17288190300252abf
Thomas Boshell <thomas.boshell@trivadis.com>
2019-10-25 13:05:01 +0200

    inital project creation

---
+++ src/main/java/com/trivadis/bth/git/bisect/playing/App.java
@@ -0,0 +1,13 @@
+package com.trivadis.bth.git.bisect.playing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
