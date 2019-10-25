b1412f9a869539c173c25662bec51599d121f753
Thomas Boshell <thomas.boshell@trivadis.com>
2019-10-25 13:24:35 +0200

    formatting

--- src/main/java/com/trivadis/bth/git/bisect/playing/App.java
+++ src/main/java/com/trivadis/bth/git/bisect/playing/App.java
@@ -1,16 +1,17 @@
 package com.trivadis.bth.git.bisect.playing;
 
 /**
- * Hello world!
+ * Hello world!  Classy clasic.
  *
  */
 public class App {
-	public static void main( String[] args )    {
-    	
-    	if(args.length>0) {
-    		System.out.println("Hello "+args[0]);
-    	}else {
-        System.out.println( "Hello World!" );
-    	}
-    }
+	
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			System.out.println("Hello" + args[0]);
+		} else {
+			System.out.println("Hello World!");
+		}
+	}
 }
