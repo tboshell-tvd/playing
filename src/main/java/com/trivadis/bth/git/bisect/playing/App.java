34de3eba219c0e3048702c17db80bf15faaa6ed1
Thomas Boshell <thomas.boshell@trivadis.com>
2019-10-25 13:21:56 +0200

    many elements

--- src/main/java/com/trivadis/bth/git/bisect/playing/App.java
+++ src/main/java/com/trivadis/bth/git/bisect/playing/App.java
@@ -4,10 +4,13 @@ package com.trivadis.bth.git.bisect.playing;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Class!" );
+public class App {
+	public static void main( String[] args )    {
+    	
+    	if(args.length>0) {
+    		System.out.println("Hello ");
+    	}else {
+        System.out.println( "Hello World!" );
+    	}
     }
 }
