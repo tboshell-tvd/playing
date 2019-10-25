e8d6bc6ab6d188a34f06f060643a196b0db482cc
Thomas Boshell <thomas.boshell@trivadis.com>
2019-10-25 13:22:47 +0200

    first one

--- src/main/java/com/trivadis/bth/git/bisect/playing/App.java
+++ src/main/java/com/trivadis/bth/git/bisect/playing/App.java
@@ -8,7 +8,7 @@ public class App {
 	public static void main( String[] args )    {
     	
     	if(args.length>0) {
-    		System.out.println("Hello ");
+    		System.out.println("Hello "+args[0]);
     	}else {
         System.out.println( "Hello World!" );
     	}
